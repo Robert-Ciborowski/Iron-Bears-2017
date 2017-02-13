fa512af85ab89e7939459175b0689cc5317e10cd
muchopotato <bobbyewy@gmail.com>
2017-02-13 08:22:39 -0500

    Made one minor change.
    Added a constant for the distance to the peg.

--- src/auto/MiddleGearPlace.java
+++ src/auto/MiddleGearPlace.java
@@ -5,9 +5,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class MiddleGearPlace extends CommandGroup {
     
+	private static final int DISTANCE_TO_PEG = 114;
+	
     public  MiddleGearPlace() {
     	Robot.chassisSubsystem.resetGyroHeading();
     	System.out.println("Gyro heading reset");
-    	addSequential(new DriveToDistance(0.4,0,74));
+    	addSequential(new DriveToDistance(0.4, 0, DISTANCE_TO_PEG));
     }
 }
